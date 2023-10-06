f8684b70c849cd89419a7c1fa662747bfe10a0d5
CutzuDev <alexfarkas05@gmail.com>
2023-10-06 21:36:17 +0300

    updt pc

--- components/sections/Stats.tsx
+++ components/sections/Stats.tsx
@@ -19,7 +19,7 @@ const StatsA: { stat: number; text: string }[] = [
 
 function Stats() {
   return (
-    <section className="flex min-h-screen w-full select-none flex-col items-center justify-start ">
+    <section className="flex  w-full select-none flex-col items-center justify-start ">
       {/* <Divider /> */}
       <motion.div
         variants={parentVariant(0.5, 0.25)}
